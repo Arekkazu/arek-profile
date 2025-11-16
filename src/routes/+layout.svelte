9919d6d873e62732d3034ae82e651152190266e2
Arekkazu <alexanderlozada230@gmail.com>
2025-11-15 20:19:09 -0500

    Testeo para nginx

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import '@sakun/system.css';
+  import "../app.css"
+  let { children } = $props();
+</script>
+
+<svelte:head>
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+
+
+<div class="grid justify-center">
+<img class=" rounded-full bg-black/45 w-60 h-60 mt-20" src="/images/arekkazu.png" alt="Avatar"/>
+
+<h2 class="title text-center">Arekkazu :D</h2>
+</div>
